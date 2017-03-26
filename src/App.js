e2fcc8835be7229ca28289daa968f90c99c12d7f
Klaus Heino <klaus.heino@gmail.com>
2017-03-27 00:42:21 +0300

    Place the container to make elements responsive within and add wrapper Also erase unnecessary code

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <p>Can you see me???</p>
+          <div className="container">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <div className="wrapper">
+            <div className="list-title">List of participants</div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
 export default App;
+
+//To get started, edit src/App.js and save to reload.
