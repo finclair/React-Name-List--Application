e107095675b46fc592bfa2a97fe541c88227e0a8
Klaus Heino <klaus.heino@gmail.com>
2017-05-17 15:16:50 +0300

    Make an AJAX request for an unlocal json file to fetch data and prepare to send if for NameList component.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ import './App.css';
 import InputFields from './Components/InputFields';
 import NameList from './Components/NameList';
 
+const data_file = "http://users.metropolia.fi/~klaush/projects/project-files/data.json";
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+     this.state = { names: [] };
+  }
+
+  componentWillMount() {
+    this.renderNames(data_file, (names) => {
+    });
+  };
+
+  renderNames(file, callback) {
+      const httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+          const response = JSON.parse(httpRequest.responseText);
+          console.log(response.Rows[0].name);
+          callback(response);
+        }
+      };
+      httpRequest.open('GET', file);
+      httpRequest.send();
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,7 +43,7 @@ class App extends Component {
           <div className="wrapper">
             <div className="list-title">List of participants</div>
             <InputFields />
-            <NameList />
+            <NameList names={ this.state.names }/>
           </div>
         </div>
       </div>
