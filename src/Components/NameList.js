759d48d7fa66580ff36925e8241f889c972b7808
Klaus Heino <klaus.heino@gmail.com>
2017-05-25 00:02:40 +0300

    Pass the dynamic data with props from component 'NameItem' to 'NameListItem' and delete previous static data

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -1,57 +1,22 @@
 import React, { Component } from 'react';
-import NameItem from './NameItem';
+import NameListItem from './NameListItem';
 
 const NameList = (props) => {
-  console.log(props.names[0]);
+  
+  const nameItems = props.names.map((name) => {
+    return <NameListItem key={name.id} name={name} />
+  });
 
-  //Tänne tulee määritellä table sekä kunkin NameItemin palautus
   return (
     <div className="name-list group">
       <div className="row ">
         <div className="name-column">Name</div>
         <div className="mail-column">E-mail address</div>
         <div className="phone-column">Phone number</div>
-        
       </div>
-      <div className="row person">
-        <div className="name-column">John Doe</div>
-        <div className="mail-column">john.doe@gmail.com</div>
-        <div className="phone-column">0405678471</div>
-        <div className="buttons-column">
-          <button className="button-modify">M</button>
-          <button className="button-delete">D</button>
-        </div>
-      </div>
-      <div className="row person">
-        <div className="name-column">George Clooney</div>
-        <div className="mail-column">george.clooney@hollywood.com</div>
-        <div className="phone-column">0504629898</div>
-        <div className="buttons-column">
-          <button className="button-modify">M</button>
-          <button className="button-delete">D</button>
-        </div>
-      </div>
-      <div className="row person">
-        <div className="name-column">George Clooney</div>
-        <div className="mail-column">george.clooney@hollywood.com</div>
-        <div className="phone-column">0504629898</div>
-        <div className="buttons-column">
-          <button className="button-modify">M</button>
-          <button className="button-delete">D</button>
-        </div>
-      </div>
-      <div className="row person">
-        <div className="name-column">George Clooney</div>
-        <div className="mail-column">george.clooney@hollywood.com</div>
-        <div className="phone-column">0504629898</div>
-        <div className="buttons-column">
-          <button className="button-modify">M</button>
-          <button className="button-delete">D</button>
-        </div>
-      </div>
+      {nameItems}
     </div>
-   
-    
+
   );
 
 };
---
+++ src/Components/NameListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NameListItem = (props) => {
+  console.log(props.name);
+  
+  const name = props.name;
+  return (
+    <div className="row person">
+      <div className="name-column">{name.name}</div>
+      <div className="mail-column">{name.e_mail}</div>
+      <div className="phone-column">{name.phone}</div>
+      <div className="buttons-column">
+        <button className="button-modify">M</button>
+        <button className="button-delete">D</button>
+      </div>
+    </div>
+  );
+      
+};
+
+export default NameListItem;
