217f67d1bf401263fba967e0a5f15656c8af57ac
Klaus Heino <klaus.heino@gmail.com>
2017-05-25 17:19:04 +0300

    Write event handlers for each input field and form

--- src/Components/InputFields.js
+++ src/Components/InputFields.js
@@ -5,17 +5,55 @@ class InputFields extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {nameInput: '', emailInput: '', phoneInput: ''};
 
+    this.onNameInputChange = this.onNameInputChange.bind(this);
+    this.onEmailInputChange = this.onEmailInputChange.bind(this);
+    this.onPhoneInputChange = this.onPhoneInputChange.bind(this);
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    
+    
+
+  }
+
+  onNameInputChange(event) {
+    
+    this.setState({ nameInput: event.target.value });
     
   }
+  onEmailInputChange(event) {
+    this.setState({ emailInput: event.target.value });
+    
+  }
+
+  onPhoneInputChange(event) {
+    this.setState({ phoneInput: event.target.value });
+    
+  }
+
+  onFormSubmit(event) {
+    
+  }
+
   render() {
     return (
       <div>
-        <form className="add-new">
+        <form className="add-new" onSubmit={this.onFormSubmit}>
           <div className="row">
-            <input className="input-field col-sm-2" placeholder="Full Name" />
-            <input className="input-field col-sm-3" placeholder="E-mail Address" />
-            <input className="input-field col-sm-2" placeholder="Phone Number" />
+            <input
+              className="input-field col-sm-2" placeholder="Full Name"
+              value={this.state.nameInput}
+              onChange={this.onNameInputChange} 
+             />
+            <input className="input-field col-sm-3" placeholder="E-mail Address" 
+              value={this.state.emailInput}
+              onChange={this.onEmailInputChange} 
+            />
+            <input className="input-field col-sm-2" placeholder="Phone Number" 
+              value={this.state.phoneInput}
+              onChange={this.onPhoneInputChange} 
+            />
             <button className="button-add col-sm-2 pull-right">Add new</button>
           </div>
         </form>
