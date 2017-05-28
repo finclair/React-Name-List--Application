b83ac32b389c882129bd50ae029114901c83a35b
Klaus Heino <klaus.heino@gmail.com>
2017-05-28 17:00:51 +0300

    Delete unnecessary elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 import InputFields from './Components/InputFields';
 import NameList from './Components/NameList';
@@ -48,10 +47,6 @@ class App extends Component {
     return (
       <div className="App">
           <div className="container">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
           <div className="wrapper">
             <div className="list-title">List of participants</div>
             <InputFields
