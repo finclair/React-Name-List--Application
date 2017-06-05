23eea744fe3fd6f6ecdbaefdc3c9e7c310343fcc
Klaus Heino <klaus.heino@gmail.com>
2017-06-05 20:53:31 +0300

    Turn EditingForm component into class based component to make it possible to set variables in states

--- src/Components/EditingForm.js
+++ src/Components/EditingForm.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const ModificationForm = (props) => {
-  console.log(props);
-  return(
-    <div>
-      <form className="form-edit">
-        <div className="row">
-            <input
-              className="input-field col-sm-2" 
-              value={props.nameEdit}
-             />
-            <input className="input-field col-sm-3" 
-              value={props.emailEdit}
-            />
-            <input className="input-field col-sm-2"  
-              value={props.phoneEdit}
-            />
-            <button className="button-edit col-sm-2 pull-right">Edit</button>
-          </div>
-        </form>
-    </div>
-  ); 
-};
+class EditingForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.nameEdit,
+      email: props.emailEdit,
+      phone: props.phoneEdit
+    }
+  }
 
-export default ModificationForm;
+  render() {
+    return(
+      <div>
+        <form className="form-edit">
+          <div className="row">
+              <input
+                className="input-field col-sm-2" 
+                value={this.state.name}
+              />
+              <input className="input-field col-sm-3" 
+                value={this.state.email}
+              />
+              <input className="input-field col-sm-2"  
+                value={this.state.phone}
+              />
+              <button className="button-edit col-sm-2 pull-right">Edit</button>
+            </div>
+          </form>
+      </div>
+    ); 
+  }
+}
+
+export default EditingForm;
