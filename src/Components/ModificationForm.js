e6128bcc369f88ffd5fcae013186fbfb59f19fad
Klaus Heino <klaus.heino@gmail.com>
2017-06-05 19:38:12 +0300

    Add markup and style

--- src/Components/ModificationForm.js
+++ src/Components/ModificationForm.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const ModificationForm = (props) => {
-
+  
+  return(
+    <div>
+      <form className="form-modify">
+        <div className="row">
+            <input className="input-field col-sm-2" placeholder="Full Name"/>
+            <input className="input-field col-sm-3" placeholder="E-mail Address" />
+            <input className="input-field col-sm-2" placeholder="Phone Number" />
+          </div>
+        </form>
+    </div>
+  ); 
 };
 
 export default ModificationForm;
