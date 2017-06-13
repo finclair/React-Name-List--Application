c406027f4cc5d5c57674d9a3159a1c8105b5de4e
Klaus Heino <klaus.heino@gmail.com>
2017-06-13 17:39:35 +0300

    Turn functional component into class based component to allow props and states

--- src/Components/NameListItem.js
+++ src/Components/NameListItem.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const NameListItem = (props) => {
+class NameListItem extends Component {
   
-  const person = props.person;
-  const deletingPerson = props.deletingPerson;
-  const showEditForm = props.showEditForm;
-  return (
-    <div className="row person">
-      <div className="name-column">{person.name}</div>
-      <div className="mail-column">{person.e_mail}</div>
-      <div className="phone-column">{person.phone}</div>
-      <div className="buttons-column">
-        <button onClick={() => showEditForm(person.id)} className="button-modify"><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
-        <button onClick={() => deletingPerson(person.id)} className="button-delete"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.person.id,
+      name: this.props.person.name,
+      e_mail: this.props.person.e_mail,
+      phone: this.props.person.phone,
+      deletingPerson: props.deletingPerson,
+    }
+    this.showEditForm = this.showEditForm.bind(this);
+
+  }
+
+  showEditForm(event) {
+    event.preventDefault();
+    this.props.showEditForm(this.state.id);
+  }
+
+  componentWillReceiveProps(newprops) {
+    console.log(newprops);
+    this.setState({
+      id: newprops.person.id,
+      name: newprops.person.name,
+      e_mail: newprops.person.e_mail,
+      phone: newprops.person.phone,
+    })
+  }
+
+  render() {
+    return (
+      <div className="row person">
+        <div className="name-column">{this.state.name}</div>
+        <div className="mail-column">{this.state.e_mail}</div>
+        <div className="phone-column">{this.state.phone}</div>
+        <div className="buttons-column">
+          <button onClick={this.showEditForm} className="button-modify"><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
+          <button onClick={() => this.state.deletingPerson(this.state.id)} className="button-delete"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+        </div>
       </div>
-    </div>
-  );
-      
+    );
+  }    
 };
 
 export default NameListItem;
